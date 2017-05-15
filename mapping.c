715d0da2280998fc579eb234987ae64bb6b423bb
Antti Alhonen <antti.alhonen@iki.fi>
2017-05-15 11:15:51 +0300

    Memory is managed for map pages; pages are stored on disk.

--- mapping.c
+++ mapping.c
@@ -4,31 +4,61 @@
 #include <string.h>
 #include <errno.h>
 
-uint32_t robot_id; // Hopefully unique identifier for the robot.
+#include "mapping.h"
+
+uint32_t robot_id = 0xacdcabba; // Hopefully unique identifier for the robot.
+
+world_t world;
 
 int cur_x, cur_y; // in mm
 
-map_page_t** mem_pages;
+void page_coords(int mm_x, int mm_y, int* pageidx_x, int* pageidx_y, int* pageoffs_x, int* pageoffs_y)
+{
+	int unit_x = mm_x / MAP_UNIT_W;
+	int unit_y = mm_y / MAP_UNIT_W;
+	unit_x += MAP_MIDDLE_UNIT;
+	unit_y += MAP_MIDDLE_UNIT;
+	int page_x = unit_x / MAP_PAGE_W;
+	int page_y = unit_y / MAP_PAGE_W;
+	int offs_x = unit_x - page_x*MAP_PAGE_W;
+	int offs_y = unit_y - page_y*MAP_PAGE_W;
+
+	*pageidx_x = page_x;
+	*pageidx_y = page_y;
+	*pageoffs_x = offs_x;
+	*pageoffs_y = offs_y;
+}
 
 int write_map_page(world_t* w, int pagex, int pagey)
 {
 	char fname[1024];
 
-	sprintf(fname, "%08x_%u_%d_%d.map", robot_id, w->id, pagex, pagey);
+	sprintf(fname, "%08x_%u_%u_%u.map", robot_id, w->id, pagex, pagey);
+
+	printf("Info: writing map page %s\n", fname);
+
 	FILE *f = fopen(fname, "w");
 	if(!f)
 	{
 		fprintf(stderr, "Error %d opening %s for write\n", errno, fname);
 		return 1;
 	}
+
+	if(fwrite(w->pages[pagex][pagey], sizeof(map_page_t), 1, f) != 1)
+	{
+		printf("Error: Writing map data failed\n");
+	}
+	fclose(f);
 	return 0;
 }
 
 int read_map_page(world_t* w, int pagex, int pagey)
 {
 	char fname[1024];
+	sprintf(fname, "%08x_%u_%u_%u.map", robot_id, w->id, pagex, pagey);
 
-	sprintf(fname, "%08x_%u_%d_%d.map", robot_id, w->id, pagex, pagey);
+	printf("Info: Attempting to read map page %s\n", fname);
+
 	FILE *f = fopen(fname, "r");
 	if(!f)
 	{
@@ -37,15 +67,120 @@ int read_map_page(world_t* w, int pagex, int pagey)
 		fprintf(stderr, "Error %d opening %s for read\n", errno, fname);
 		return 1;
 	}
+
+	if(fread(w->pages[pagex][pagey], sizeof(map_page_t), 1, f) != 1)
+	{
+		printf("Error: Reading map data failed\n");
+	}
+
+	fclose(f);
 	return 0;
+}
+
+int load_map_page(world_t* w, int pagex, int pagey)
+{
+	if(w->pages[pagex][pagey])
+	{
+		printf("Info: reloading already allocated map page %d,%d\n", pagex, pagey);
+	}
+	else
+	{
+		printf("Info: Allocating mem for page %d,%d\n", pagex, pagey);
+		w->pages[pagex][pagey] = malloc(sizeof(map_page_t));
+	}
+
+	int ret = read_map_page(w, pagex, pagey);
+	if(ret == 2)
+	{
+		printf("Info: map file didn't exist, initializing empty map\n");
+		memset(w->pages[pagex][pagey], 0, sizeof(map_page_t));
+	}
+	else if(ret)
+	{
+		printf("Error: Reading map file failed. Initializing empty map\n");
+		memset(w->pages[pagex][pagey], 0, sizeof(map_page_t));
+	}
+}
+
+int unload_map_page(world_t* w, int pagex, int pagey)
+{
+	if(w->pages[pagex][pagey])
+	{
+		if(write_map_page(w, pagex, pagey))
+		{
+			printf("Error: writing map page (%d,%d) to disk failed\n", pagex, pagey);
+		}
+		printf("Info: Freeing mem for page %d,%d\n", pagex, pagey);
+		free(w->pages[pagex][pagey]);
+		w->pages[pagex][pagey] = 0;
+	}
+	else
+	{
+		printf("Warn: Trying to unload a map page which is already free.\n");
+	}
+}
+
+int unload_map_pages(world_t* w)
+{
+	int idx_x = 0, idx_y = 0, offs_x = 0, offs_y = 0;
+
+	page_coords(cur_x, cur_y, &idx_x, &idx_y, &offs_x, &offs_y);
+
+	for(int x = 0; x < MAP_W; x++)
+	{
+		for(int y = 0; y < MAP_W; y++)
+		{
+			if(w->pages[x][y] && (abs(idx_x - x) > 2 || abs(idx_y - y) > 2))
+			{
+				unload_map_page(w, x, y);
+			}
+
+		}
+	}
+}
+
+void load_9pages(world_t* w, int pagex, int pagey)
+{
+	for(int x=-1; x<2; x++)
+	{
+		for(int y=-1; y<2; y++)
+		{
+			int xx = pagex+x;
+			int yy = pagey+y;
+			if(!world.pages[xx][yy])
+			{
+				load_map_page(&world, xx, yy);
+			}
+		}
+	}
 }
 
 int main(int argc, char** argv)
 {
 
+	int mm_x = 0;
+	int mm_y = 0;
+	while(1)
+	{
+
+		int idx_x, idx_y, offs_x, offs_y;
+
+		page_coords(mm_x, mm_y, &idx_x, &idx_y, &offs_x, &offs_y);
+
+		printf("Page X %d  Page Y %d  Offs X %d  Offs Y %d\n\n", idx_x, idx_y, offs_x, offs_y);
+
+		load_9pages(&world, idx_x, idx_y);
+		
+		unload_map_pages(&world);
+
+		printf("x?\n");
+		scanf("%d", &mm_x);
+		printf("y?\n");
+		scanf("%d", &mm_y);
+
+	}
 
 
-	free(mem_pages);
 	return 0;
 }
 
--- mapping.h
+++ mapping.h
@@ -8,6 +8,8 @@
 #define UNIT_ITEM	(1<<0)	// Small obstacle, detected by sonars or bumping into it
 #define UNIT_WALL	(1<<1)	// Obstacle seen by the lidar.
 
+#define CONSTRAINT_FORBIDDEN 	(1<<0)	// "Don't go here" unit
+
 
 /*
 Map unit is a 40mm*40mm area. Memory usage is carefully considered, because the world is a 2D map of the map units.
@@ -17,17 +19,17 @@ tight passages, which would be wasteful if the unit was larger) and memory/disk 
 
 typedef struct __attribute__ ((packed))
 {
-	uint16_t timestamp;	// Latest time mapped
+	uint8_t result;   	// Mapping result decided based on all available data.
+	uint8_t latest;  	// Mapping result based on last scan.
 
-	uint8_t latest;  	// Latest mapping result
+	uint16_t timestamp;	// Latest time scanned
+
 	uint8_t num_seen;  	// Number of times mapped. Saturated at 255.
-
 	uint8_t num_obstacles;  // "is an obstacle" counter. Every time mapped, ++ if obstacle, -- if not. Saturated at 255.
-	uint8_t reserved;
 
-	uint16_t reserved;
-	
+	uint8_t constraints;
 } map_unit_t;
+
 
 /*
 Map page is a fixed 256*256 * 40mm*40mm = 10.24m*10.24m area.
@@ -43,10 +45,26 @@ To satisfy these conditions, map pages are stored/retrieved to/from the disk eve
 #define MAP_PAGE_W 256 // in map_units
 #define MAP_PAGE_W_MM (MAP_UNIT_W * MAP_PAGE_W)  // convenience define
 
+
+/*
+Quick map only holds the mapping result byte, and with half*half resolution; 1 map page will be 16Kbytes instead of 0.5Mbytes.
+*/
+
+#define QMAP_UNIT_W 80 // in mm
+#define QMAP_PAGE_W 128
+#define QMAP_PAGE_W_MM (QMAP_UNIT_W * QMAP_PAGE_W)
+
+
 typedef struct
 {
 	map_unit_t units[MAP_PAGE_W][MAP_PAGE_W];
 } map_page_t;
+
+
+typedef struct
+{
+	uint8_t units[QMAP_PAGE_W][QMAP_PAGE_W];
+} qmap_page_t;
 
 
 
@@ -56,12 +74,28 @@ in case they would, they should be combined.
 
 When the software cannot decide which world it's in, we create a new, empty world. If we figure out it matches
 an earlier world, we will combine them.
+
+To make things simple, we statically allocate for the map page pointers. Map pages themselves are dynamically allocated.
+
+256*256 map pages will limit the maximum world size to 2.62 km * 2.62 km. On a 64-bit system, storing pointers to map pages
+and qmap pages, 1Mbyte is required for the pointers. NULL pointer means the data is not loaded in memory.
+
 */
+
+#define MAP_W 256
+#define MAP_MIDDLE_PAGE (MAP_W/2)
+
+#define MAP_MIDDLE_UNIT (MAP_PAGE_W * MAP_MIDDLE_PAGE)
+
 typedef struct
 {
 	uint32_t id;
+
+	map_page_t*  pages[MAP_W][MAP_W];
+	qmap_page_t* qpages[MAP_W][MAP_W];
+
 } world_t;
 
-
+void page_coords(int mm_x, int mm_y, int* pageidx_x, int* pageidx_y, int* pageoffs_x, int* pageoffs_y);
 
 #endif

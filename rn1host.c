83ea4d1d4951c097d227aad7770f626c434f03fa
Antti Alhonen <antti.alhonen@iki.fi>
2017-05-24 16:35:09 +0300

    bugfix

--- rn1host.c
+++ rn1host.c
@@ -109,7 +109,7 @@ int main(int argc, char** argv)
 		lidar_scan_t* p_lid;
 		if( (p_lid = get_lidar()) )
 		{
-			tcp_send_hwdbg(hwdbg);
+			if(tcp_client_sock >= 0) tcp_send_hwdbg(hwdbg);
 
 			static int lidar_cnt = 0;
 

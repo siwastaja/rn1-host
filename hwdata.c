aada366694a8df63a14b7e568eebc0c0ff173756
Antti Alhonen <antti.alhonen@iki.fi>
2017-06-12 10:14:21 +0300

    dbg

--- hwdata.c
+++ hwdata.c
@@ -217,6 +217,8 @@ void move_to(int32_t x, int32_t y, int8_t backmode, int id)
 		id = 0;
 	}
 
+	printf("INFO: Move to (%d, %d), backmode=%d, id=%d\n", x,y,backmode,id);
+
 	buf[0] = 0x82;
 	buf[1] = I32_I7_4(x);
 	buf[2] = I32_I7_3(x);

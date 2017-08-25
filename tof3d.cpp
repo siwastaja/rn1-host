14f299ae705f43e24b7be80eb9d07aa203fe97c4
Antti Alhonen <antti.alhonen@iki.fi>
2017-08-25 10:46:30 +0300

    better body ignore during 3dtof calibration

--- tof3d.cpp
+++ tof3d.cpp
@@ -542,20 +542,39 @@ void Softkinetic_tof::onNewDepthNodeSampleReceived(DepthSense::DepthNode node, D
 			{
 				for(int sy = 3; sy < TOF3D_HMAP_YSPOTS-3; sy++)
 				{
-					if(hmap_calib[sx][sy] > avg + 80)
+					if(hmap_calib[sx][sy] > avg + 70)
 					{
 						body_ignores++;
 						hmap_calib[sx+0][sy+0] = -9999;
 						hmap_calib[sx+0][sy-1] = -9999;
 						hmap_calib[sx+0][sy+1] = -9999;
+						hmap_calib[sx+0][sy-2] = -9999;
+						hmap_calib[sx+0][sy+2] = -9999;
 						hmap_calib[sx+1][sy+0] = -9999;
 						hmap_calib[sx+1][sy-1] = -9999;
 						hmap_calib[sx+1][sy+1] = -9999;
+						hmap_calib[sx+1][sy-2] = -9999;
+						hmap_calib[sx+1][sy+2] = -9999;
+						hmap_calib[sx+2][sy+0] = -9999;
+						hmap_calib[sx+2][sy-1] = -9999;
+						hmap_calib[sx+2][sy+1] = -9999;
+						hmap_calib[sx+2][sy-2] = -9999;
+						hmap_calib[sx+2][sy+2] = -9999;
 						if(sx > 0)
 						{
 							hmap_calib[sx-1][sy+0] = -9999;
 							hmap_calib[sx-1][sy-1] = -9999;
 							hmap_calib[sx-1][sy+1] = -9999;
+							hmap_calib[sx-1][sy-2] = -9999;
+							hmap_calib[sx-1][sy+2] = -9999;
+						}
+						if(sx > 1)
+						{
+							hmap_calib[sx-2][sy+0] = -9999;
+							hmap_calib[sx-2][sy-1] = -9999;
+							hmap_calib[sx-2][sy+1] = -9999;
+							hmap_calib[sx-2][sy-2] = -9999;
+							hmap_calib[sx-2][sy+2] = -9999;
 						}
 					}
 				}

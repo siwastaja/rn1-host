6a68769a65b0daddae92cf1f1eca08f61c060ed8
Antti Alhonen <antti.alhonen@iki.fi>
2018-03-06 17:25:09 +0200

    geometry calibration in progress

--- pulutof.c
+++ pulutof.c
@@ -146,22 +146,104 @@ pulutof_frame_t* get_pulutof_frame()
 	return ret;
 }
 
+
+static float x_angs[TOF_XS*TOF_YS];
+static float y_angs[TOF_XS*TOF_YS];
+
+#define GEOCAL_N_X 5
+#define GEOCAL_N_Y 6
+
+typedef struct geocal_point_t
+{
+	int sens_x;
+	int sens_y;
+	float ang_x;
+	float ang_y;
+};
+
+/*
+	Pixel coordinates
+*/
+static const geocal_point_t lens_quadrant_coords[GEOCAL_N_Y+1][GEOCAL_N_X+1] =
+{
+	{ { 12,   2,   50.0, 25.0}, { 19,   1,   45.0, 25.0}, { 28,   0,   40.0, 25.0}, { 43,  -1,   30.0, 25.0}, { 62,  -2,   15.0, 25.0}, { 80,  -2,   0, 25.0} },
+	{ { 11,   5,   50.0, 22.5}, { 19,   4,   45.0, 22.5}, { 27,   3,   40.0, 22.5}, { 42,   2,   30.0, 22.5}, { 62,   1,   15.0, 22.5}, { 80,   1,   0, 22.5} },
+	{ { 11,   8,   50.0, 20.0}, { 19,   6,   45.0, 20.0}, { 27,   6,   40.0, 20.0}, { 42,   5,   30.0, 20.0}, { 62,   4,   15.0, 20.0}, { 80,   4,   0, 20.0} },
+	{ { 10,  10,   50.0, 17.5}, { 19,   9,   45.0, 17.5}, { 27,   9,   40.0, 17.5}, { 42,   8,   30.0, 17.5}, { 62,   8,   15.0, 17.5}, { 80,   8,   0, 17.5} },
+	{ { 10,  13,   50.0, 15.0}, { 19,  12,   45.0, 15.0}, { 27,  12,   40.0, 15.0}, { 42,  11,   30.0, 15.0}, { 62,  11,   15.0, 15.0}, { 80,  11,   0, 15.0} },
+	{ { 10,  19,   50.0, 10.0}, { 19,  18,   45.0, 10.0}, { 26,  18,   40.0, 10.0}, { 42,  17,   30.0, 10.0}, { 62,  17,   15.0, 10.0}, { 80,  17,   0, 10.0} },
+	{ { 10,  29,   50.0,  0.0}, { 19,  29,   45.0,  0.0}, { 26,  29,   40.0,  0.0}, { 42,  29,   30.0,  0.0}, { 62,  29,   15.0,  0.0}, { 80,  29,   0,  0.0} }
+};
+
+static float x_angs[TOF_XS*TOF_YS];
+static float y_angs[TOF_XS*TOF_YS];
+
+static void outp_ang(int px, int py, float ax, float ay)
+{
+	if(py < 0 || py > TOF_YS-1 || px < 0 || px > TOF_XS-1)
+		return;
+
+	x_angs[py*TOF_XS+px] = ax;
+	y_angs[py*TOF_XS+px] = ay;
+}
+
+static void gen_ang_tables()
+{
+	int cyy = 0;
+	for(int calyy=0; calyy<GEOCAL_N_Y*2+1; calyy++)
+	{
+		// Left half, extrapolate the first X record
+		{
+			float dpx = lens_quadrant_coords[cyy][1].sens_x - lens_quadrant_coords[cyy][0].sens_x;
+			float dpy = lens_quadrant_coords[cyy][1].sens_y - lens_quadrant_coords[cyy][0].sens_y;
+			float dax = lens_quadrant_coords[cyy][1].ang_x - lens_quadrant_coords[cyy][0].ang_x;
+			float ax_per_px = dax/dpx;
+			float py_per_px = dpy/dpx;
+			float cur_ax = -1*lens_quadrant_coords[cyy][0].ang_x - ax_per_px*lens_quadrant_coords[cyy][0].sens_x;
+			float cur_ay = -1*lens_quadrant_coords[cyy][0].ang_y;
+			float cur_py = lens_quadrant_coors[cyy][0].sens_y - py_per_px*lens_quadrant_coords[cyy][0].sens_x;
+			for(int pxx=0; pxx<lens_quadrant_coords[cyy][0].sens_x; pxx++)
+			{
+				outp_ang(pxx, pyy, cur_ax, cur_ay);
+			}
+		}
+		for(int calxx=0; calxx<GEOCAL_N_X*2+1; calxx++)
+		{
+			
+		}
+
+
+		if(calyy < GEOCAL_N_Y+1)
+			cyy++;
+		else
+			cyy--;
+	}
+
+	for(int yy=0; yy<TOF_YS; yy++)
+	{
+		for(int xx=0; xx<TOF_XS; xx++)
+		{
+			// Find nearest calibration point in x dir
+		}
+	}
+}
+
 /*
 	As you should know, SPI is a "show yours, I'll show mine" protocol. As a master, we'll only send something, and get something back.
 	Unfortunately, Raspberry Pi can only be a master, in SPI terms.
 	However, the PULUTOF in kind of a "real" master, because it decides when to image the frames, and will do it regardless of what we are
 	doing on the Raspberry - the data is anyway needed for low-level obstacle avoidance, completely independent of the Raspi.
 
-	poll_availability commands a small (one-byte) transfer, which will only return the first status byte. If we are happy with the status (i.e., there is
-	data available), we can go on with the full transfer. This data further gives insight how long to wait before next polling is needed.
+	poll_availability commands a small (five-byte) transfer, which will only return the first status byte. If we are happy with the status (i.e., there is
+	data available), we can go on with the full transfer. This status byte further gives insight how long to wait before next polling is needed.
 
 	When poll_availability informs there is a frame available, read_frame can be called - it assumes the data is indeed available and performs a full
-	frame read (if the data is not available, you'll hit an old frame, or possibly a corrupted combination of two frames).
+	frame read (if the data is not available - the status byte clearly tells this, but you still read it, you'll hit an old frame, or
+	possibly a corrupted combination of two frames).
 */
 
 
 static uint8_t txbuf[65536];
-
 
 static int poll_availability()
 {

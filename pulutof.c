d7f02197b642fca16ed335f37bcd6a60a898478e
Antti Alhonen <antti.alhonen@iki.fi>
2018-03-21 16:20:10 +0200

    moar dbg info

--- pulutof.c
+++ pulutof.c
@@ -1081,7 +1081,7 @@ static int read_frame()
 	}
 
 #ifdef SPI_PRINT_DBG
-	printf("Frame (sensor_idx= %d) read ok, pose=(%d,%d,%d). Timing data:\n", pulutof_ringbuf[pulutof_ringbuf_wr].sensor_idx, pulutof_ringbuf[pulutof_ringbuf_wr].robot_pos.x, pulutof_ringbuf[pulutof_ringbuf_wr].robot_pos.y, pulutof_ringbuf[pulutof_ringbuf_wr].ang);
+	printf("Frame (sensor_idx= %d) read ok, pose=(%d,%d,%d). Timing data:\n", pulutof_ringbuf[pulutof_ringbuf_wr].sensor_idx, pulutof_ringbuf[pulutof_ringbuf_wr].robot_pos.x, pulutof_ringbuf[pulutof_ringbuf_wr].robot_pos.y, pulutof_ringbuf[pulutof_ringbuf_wr].robot_pos.ang);
 	for(int i=0; i<24; i++)
 	{
 		printf("%d:%.1f ", i, (float)pulutof_ringbuf[pulutof_ringbuf_wr].timestamps[i]/10.0);
